2929397de2af3828961108b5c7fba3f3312007ce
suman98 <41943773+suman98@users.noreply.github.com>
2024-10-01 08:21:24 +0545

    added playground

--- src/layouts/Router.tsx
+++ src/layouts/Router.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/home/index';
+import PlayGround from '@/pages/play';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/play" element={<PlayGround />} />
       </Routes>
     </Router>
   );
